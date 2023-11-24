7c053fc2f6c4fd69afbdcda89d86977f2e70ee68
Mauricio Agurcia <104690999+Icho2@users.noreply.github.com>
2023-11-24 00:33:15 -0600

    yes

--- Project#01/solution.cpp
+++ Project#01/solution.cpp
@@ -2,6 +2,27 @@
 #include <fstream>
 #include <iomanip>
 #include <list>
+#include <string>
+
+class Molecule
+{
+  public:
+    int natom;
+    int charge;
+    int *zvals;
+    double **geom;
+    string point_group;
+ 
+    void print_geom();
+    void rotate(double phi);
+    void translate(double x, double y, double z);
+    double bond(int atom1, int atom2);
+    double angle(int atom1, int atom2, int atom3);
+    double torsion(int atom1, int atom2, int atom3, int atom4);
+ 
+    Molecule();
+    ~Molecule();
+};
 
 int main(){
 //Step 1
