3635ef09b542b8a64e8ee0cb192b298e28af996b
Mauricio Agurcia <104690999+Icho2@users.noreply.github.com>
2023-11-24 00:51:07 -0600

    got rid of moelcule class

--- Project#01/solution.cpp
+++ Project#01/solution.cpp
@@ -3,31 +3,11 @@
 #include <iomanip>
 #include <list>
 #include <string>
-
-class Molecule
-{
-  public:
-    int natom;
-    int charge;
-    int *zvals;
-    double **geom;
-    string point_group;
- 
-    void print_geom();
-    void rotate(double phi);
-    void translate(double x, double y, double z);
-    double bond(int atom1, int atom2);
-    double angle(int atom1, int atom2, int atom3);
-    double torsion(int atom1, int atom2, int atom3, int atom4);
- 
-    Molecule();
-    ~Molecule();
-};
+#include <math>
 
 int main(){
 //Step 1
 	std::ifstream input("acetaldehyde.dat");
-	double **R = new double* [mol.natom];
 	double pos;
 	int natom;
 	double numero;
@@ -56,12 +36,10 @@ int main(){
 
 //Step 2
 	double xij, yij, zij;
-	for(int i=0; i < mol.natom; i++){
-  		R[i] = new double[mol.natom];
-	}
+	double R[natom][natom];
 	
-	for(int i=0; i < mol.natom; i++){
-		for(int j=0; j < mol.atom; j++){
+	for(int i=0; i < natom; i++){
+		for(int j=0; j < natom; j++){
 			xij = x_vect[i] - x_vect[j];
 			yij = y_vect[i] - y_vect[j];
 			zij = z_vect[i] - z_vect[j];;
