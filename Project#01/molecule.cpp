184d1d79a28d9c67bbb64e4ea85b7134d4b304ab
Icho2 <Mauricio.agurcia@utexas.edu>
2023-12-25 11:48:01 -0600

    something

--- Project#01/molecule.cpp
+++ Project#01/molecule.cpp
@@ -14,7 +14,7 @@ molecule::molecule(std::string molecule_file){
 		std::ifstream input(molecule_file);
 	    	if (!input.is_open()){
 			std::cerr << "Uninstall yourself.\n";
-	        	return 1;
+	        	return;
 	    	}
 		int natom; //number of atoms in the molecule
         	input >> natom;
@@ -23,8 +23,8 @@ molecule::molecule(std::string molecule_file){
   			std::vector<double> x_vect(natom); // x values
   			std::vector<double> y_vect(natom); // y ''
   			std::vector<double> z_vect(natom); // z ''
-        	std::cout << "Number of atoms: " << natom << "\n";
-	    	std::cout << "Input Cartesian coordinates:\n";
+        	//std::cout << "Number of atoms: " << natom << "\n";
+	    	//std::cout << "Input Cartesian coordinates:\n";
 	    	for(int i=0; i < natom; i++)
 	    	{
 			input >> numero;
@@ -35,41 +35,40 @@ molecule::molecule(std::string molecule_file){
             		y_vect[i] = numero;
             		input >> std::fixed >> std::setprecision(12) >> numero;
             		z_vect[i]= numero;
-            		printf("%d %20.12f %20.12f %20.12f\n", (int) Z_val[i], x_vect[i], y_vect[i], z_vect[i]);
+            		//printf("%d %20.12f %20.12f %20.12f\n", (int) Z_val[i], x_vect[i], y_vect[i], z_vect[i]);
         	}		
 
     	input.close();
-
 }
 
 int molecule::display_coordinates(){
         //Step 1: Read the Coordinate Data from Input
         //check if you are able to open up the molcule file first
-		std::ifstream input(molecule_info);
-	    	if (!input.is_open()){
-			std::cerr << "Uninstall yourself.\n";
-	        	return 1;
-	    	}
+		//std::ifstream input(molecule_info);
+	    //	if (!input.is_open()){
+		//	std::cerr << "Uninstall yourself.\n";
+	    //   	return 1;
+	    //	}
 
-			int natom; //number of atoms in the molecule
-        	input >> natom;
-			double numero; //using it to read the inputs of the molecule file
-			std::vector<int> Z_val(natom); //atomic number of a said atom
-  			std::vector<double> x_vect(natom); // x values
-  			std::vector<double> y_vect(natom); // y ''
-  			std::vector<double> z_vect(natom); // z ''
+		//	int natom; //number of atoms in the molecule
+        //	input >> natom;
+		//	double numero; //using it to read the inputs of the molecule file
+		//	std::vector<int> Z_val(natom); //atomic number of a said atom
+  		//	std::vector<double> x_vect(natom); // x values
+  		//	std::vector<double> y_vect(natom); // y ''
+  		//	std::vector<double> z_vect(natom); // z ''
         	std::cout << "Number of atoms: " << natom << "\n";
 	    	std::cout << "Input Cartesian coordinates:\n";
 	    	for(int i=0; i < natom; i++)
 	    	{
 			input >> numero;
 		    	Z_val[i] = numero;
-            		input >> std::fixed >> std::setprecision(12) >> numero;
-            		x_vect[i] = numero;
-            		input >> std::fixed >> std::setprecision(12) >> numero;
-            		y_vect[i] = numero;
-            		input >> std::fixed >> std::setprecision(12) >> numero;
-            		z_vect[i]= numero;
+            		//input >> std::fixed >> std::setprecision(12) >> numero;
+            		//x_vect[i] = numero;
+            		//input >> std::fixed >> std::setprecision(12) >> numero;
+            		//y_vect[i] = numero;
+            		//input >> std::fixed >> std::setprecision(12) >> numero;
+            		//z_vect[i]= numero;
             		printf("%d %20.12f %20.12f %20.12f\n", (int) Z_val[i], x_vect[i], y_vect[i], z_vect[i]);
         	}		
 
--- Project#01/molecule.h
+++ Project#01/molecule.h
@@ -5,11 +5,15 @@
 
 class molecule {
 public:
+        int natom;
+    int numero;
+	std::vector<int> Z_val(natom); //atomic number of a said atom
+    std::vector<double> x_vect(natom); // x values
+    std::vector<double> y_vect(natom); // y ''
+  	std::vector<double> z_vect(natom); // z ''
+};
     molecule(std::string xyz_file);
 
     int display_coordinates();
-
-private:
-
-};
+    
 #endif
