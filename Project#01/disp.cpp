39cd9cac392720e84140a98ba1ef63593bd87b5a
Icho2 <Mauricio.agurcia@utexas.edu>
2024-01-11 20:29:46 -0600

    my message

--- Project#01/disp.cpp
+++ Project#01/disp.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include "molecule.h"
+#include <fstream>
 
 int main(){
 	
+	std::ifstream file;
+	file.open("acetaldehyde.dat");
 	molecule acetaldehyde;
 	
 	int natom;
+	file >> natom;
+	std::cout << natom << "\n";
 	int charge;
 	int numero;
 	int *z_val;
 	double geom;
 	std::string point_group;
-
-	acetaldehyde.disp_coordinates();
+	
+	//acetaldehyde.disp_coordinates();
 	
 	return 0;
 }
--- Project#01/molecule.cpp
+++ Project#01/molecule.cpp
@@ -4,7 +4,7 @@
 int disp_coordinates()
 {
 	for(int i=0; i < natom; i++)
-    printf("%d %8.5f %8.5f %8.5f\n", z_val[i], geom[i][0], geom[i][1], geom[i][2]);
+    	{printf("%d %8.5f %8.5f %8.5f\n", z_val[i], geom[i][0], geom[i][1], geom[i][2]);}
 }
 
 molecule::molecule(){ }
