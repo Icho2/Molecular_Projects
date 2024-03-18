2d39dc4110651a9801a9f20a75f8224cdac1d98e
Icho2 <Mauricio.agurcia@utexas.edu>
2024-03-18 17:01:15 -0500

    commited from my home PC

--- Project#01/part1.cpp
+++ Project#01/part1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 using std::endl;
 using std::cout;
@@ -8,6 +9,7 @@ using std::string;
 using std::ios;
 
 int main(){
+// PART 1
 	FILE *my_input;		
 	string line;
 	int natom;
@@ -23,5 +25,16 @@ int main(){
 	for(int i=0; i<natom; i++)
 		printf("%d %lf %lf %lf \n",  zval[i], coordinates[i][0], coordinates[i][1], coordinates[i][2]);
 	
+//PART 2
+	double R[natom][natom];	
+	double x, y, z;
+	for(int i=0; i < natom; i++){
+		for(int j=0; j < natom; j++){
+			x = coordinates[i][0] - coordinates[j][0];
+			y = coordinates[i][1] - coordinates[j][1];
+			z = coordinates[i][2] - coordinates[j][2];
+			R[i][j] = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+		}
+	}
 	return 0;	
 }
