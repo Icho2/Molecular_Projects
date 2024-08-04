03ce83e331e1d0e53ff1c4e68286168a2910a41e
Icho2 <Mauricio.agurcia@utexas.edu>
2024-08-03 21:02:09 -0500

    commited from my home PC

--- Project#01/molecule.cpp
+++ Project#01/molecule.cpp
@@ -70,29 +70,37 @@ double Molecule::oop_angle(int atom1, int atom2, int atom3, int atom4)
 	double ckjjl[3];
 	double theta;
 	
-	ekj[0] = (geom[atom1][0] - geom[atom2][0])/bond(atom3, atom2);
-        ekj[1] = (geom[atom1][1] - geom[atom2][1])/bond(atom3, atom2);
-        ekj[2] = (geom[atom1][2] - geom[atom2][2])/bond(atom3, atom2);
+	ekj[0] = -(geom[atom3][0] - geom[atom2][0])/bond(atom3, atom2);
+        ekj[1] = -(geom[atom3][1] - geom[atom2][1])/bond(atom3, atom2);
+        ekj[2] = -(geom[atom3][2] - geom[atom2][2])/bond(atom3, atom2);
 	
-	ekl[0] = (geom[atom1][0] - geom[atom2][0])/bond(atom3, atom4);
-        ekl[1] = (geom[atom1][1] - geom[atom2][1])/bond(atom3, atom4);
-        ekl[2] = (geom[atom1][2] - geom[atom2][2])/bond(atom3, atom4);
+	ekl[0] = -(geom[atom3][0] - geom[atom4][0])/bond(atom3, atom4);
+        ekl[1] = -(geom[atom3][1] - geom[atom4][1])/bond(atom3, atom4);
+        ekl[2] = -(geom[atom3][2] - geom[atom4][2])/bond(atom3, atom4);
 	
-	eki[0] = (geom[atom1][0] - geom[atom2][0])/bond(atom3, atom1);
-        eki[1] = (geom[atom1][1] - geom[atom2][1])/bond(atom3, atom1);
-        eki[2] = (geom[atom1][2] - geom[atom2][2])/bond(atom3, atom1);
+	eki[0] = -(geom[atom3][0] - geom[atom1][0])/bond(atom3, atom1);
+        eki[1] = -(geom[atom3][1] - geom[atom1][1])/bond(atom3, atom1);
+        eki[2] = -(geom[atom3][2] - geom[atom1][2])/bond(atom3, atom1);
 	
+	//cout << "ekj:" << endl;
+	//cout << ekj[0] << endl;
+	//cout << ekj[1] << endl;
+	//cout << ekj[2] << endl;
+	
+	//cross product
 	c1 = (ekj[1]*ekl[2]) - (ekj[2]*ekl[1]);
 	c2 = (ekj[2]*ekl[0]) - (ekj[0]*ekl[2]);
 	c3 = (ekj[0]*ekl[1]) - (ekj[1]*ekl[0]);
 	
+	//dividing by sin theta
 	ckjjl[0] = c1 / sin(angle(atom2, atom3, atom4));
 	ckjjl[1] = c2 / sin(angle(atom2, atom3, atom4));
 	ckjjl[2] = c3 / sin(angle(atom2, atom3, atom4));
 	
+	//dot product
 	theta = asin((ckjjl[0]*eki[0]) + (ckjjl[1]*eki[1]) + (ckjjl[2]*eki[2])); 
 	
-	return theta;
+	return theta*(180.0 / acos(-1.0));
 }
 //constructor
 Molecule::Molecule(const char* filename, int q){
